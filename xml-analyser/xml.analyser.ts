2e778caf74294428b464b8254ae34a7e592e0cb8
nick_richardson <nick.richardson@rezonence.com>
2021-08-07 15:53:36 +0100

    Filter out all none post data nodes

--- xml-analyser/xml.analyser.ts
+++ xml-analyser/xml.analyser.ts
@@ -4,7 +4,7 @@ import {PostData} from "./post.data";
 export class XmlAnalyser {
 
     toXmlAnalysis(nodes: any[]): XmlAnalysis {
-        const postData: PostData[] = nodes.filter(node => !!node.Id);
+        const postData: PostData[] = nodes.filter(node => this.isPostDataNode(node));
         const details = {
             firstPost: postData[0].CreationDate,
             lastPost: postData[postData.length - 1].CreationDate,
@@ -13,6 +13,10 @@ export class XmlAnalyser {
             avgScore: this.toAverageScore(postData)
         };
         return {analyseDate: new Date().toISOString(), details};
+    }
+
+    private isPostDataNode(node: any): boolean {
+        return node.Id && node.CreationDate;
     }
 
     private toAverageScore(nodes: PostData[]): number {

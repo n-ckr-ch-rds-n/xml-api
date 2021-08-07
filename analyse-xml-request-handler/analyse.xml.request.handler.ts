46e7788451279f738ac0a13b3f7703c91018a660
nick_richardson <nick.richardson@rezonence.com>
2021-08-07 17:05:25 +0100

    Handle errors on both streams

--- analyse-xml-request-handler/analyse.xml.request.handler.ts
+++ analyse-xml-request-handler/analyse.xml.request.handler.ts
@@ -1,8 +1,9 @@
 import got from "got";
 import {Parser} from "node-expat";
 import {XmlAnalyser} from "../xml-analyser/xml.analyser";
-import {waitFor, waitForFirst} from "wait-for-event";
+import {waitForFirst} from "wait-for-event";
 import {XmlAnalysis} from "../xml-analyser/xml.analysis";
+import {Readable} from "stream";
 
 export class AnalyseXmlRequestHandler {
 
@@ -14,12 +15,23 @@ export class AnalyseXmlRequestHandler {
     async handle(url: string): Promise<XmlAnalysis> {
         this.parser.on("startElement", (name, attrs) => this.nodes.push(attrs));
         const xmlStream = got.stream(url);
-        xmlStream.on("error", () => xmlStream.emit("close"));
         const parsedXmlStream = xmlStream.pipe(this.parser);
+        for (const stream of [xmlStream, parsedXmlStream]) {
+            stream.on("error", (e) => this.handleError(e, stream as Readable));
+        }
         await waitForFirst("close", [xmlStream, parsedXmlStream]);
         const analysis = this.analyser.toXmlAnalysis(this.nodes);
+        this.cleanup();
+        return analysis;
+    }
+
+    private cleanup(): void {
         this.nodes = [];
         this.parser.reset();
-        return analysis;
+    }
+
+    private handleError(error: Error, stream: Readable): void {
+        console.error(error);
+        stream.emit("close");
     }
 }
